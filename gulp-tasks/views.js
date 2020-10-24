53cf608acf7c1212c659728263c890d0f324f9d4
x-dew <69053445+x-dew@users.noreply.github.com>
2020-10-24 10:45:56 +0300

    car

---
+++ gulp-tasks/views.js
@@ -0,0 +1,25 @@
+"use strict";
+
+import { paths } from "../gulpfile.babel";
+import gulp from "gulp";
+import include from "gulp-file-include";
+import gulpif from "gulp-if";
+import replace from "gulp-replace";
+import browsersync from "browser-sync";
+import yargs from "yargs";
+
+const argv = yargs.argv,
+    production = !!argv.production;
+
+gulp.task("views", () => {
+    return gulp.src(paths.views.src)
+        .pipe(include({
+            prefix: "@@",
+            basepath: "@file"
+        }))
+        .pipe(gulpif(production, replace(".css", ".min.css")))
+        .pipe(gulpif(production, replace(".js", ".min.js")))
+        .pipe(replace("../../../img/", "img/"))
+        .pipe(gulp.dest(paths.views.dist))
+        .pipe(browsersync.stream());
+});
